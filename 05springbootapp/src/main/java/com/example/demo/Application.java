685b830e231589d0c4a0478fe3078213c1b2e85e
teckwaseem <WT@gmail.com>
2021-03-02 19:16:00 +0530

    add m1() in my class application

--- 05springbootapp/src/main/java/com/example/demo/Application.java
+++ 05springbootapp/src/main/java/com/example/demo/Application.java
@@ -5,9 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class Application {
+	
+	public static void m1() {
+		System.out.println("write is coloun");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+		m1();
 	}
 
 }
